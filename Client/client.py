b52d64794fbd3c1673bb53daf0a4d86654ad8e8a
Sergio Sierra <sd.sierra@uniandes.edu.co>
2021-10-15 10:32:29 -0500

    Initial Repo

---
+++ Client/client.py
@@ -0,0 +1,100 @@
+from socket import socket
+from threading import Thread
+from hashlib import sha256
+from time import time
+from datetime import datetime
+from os import path
+BUFFER_SIZE = 1024
+
+
+class ThreadServidor(Thread):
+    def __init__(self, id, socket):
+        Thread.__init__(self)
+        self.id = id
+        self.socket = socket
+        self.numeroConexiones = ""
+        self.nArchivo = ""
+        self.hashCalculado = ""
+        self.hashServidor = ""
+        self.startTime = None
+        self.tiempoTotal = None
+
+    def run(self):
+        self.socket.send("ImReadyServer".encode())
+        self.id = self.socket.recv(BUFFER_SIZE).decode()
+        self.numeroConexiones = self.socket.recv(BUFFER_SIZE).decode()
+        self.nArchivo = self.socket.recv(BUFFER_SIZE).decode()
+        self.hashServidor = self.socket.recv(BUFFER_SIZE)
+        date = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        file1 = open(
+            f"ArchivosRecibidos/{date}-Cliente{self.id}-Prueba-{self.numeroConexiones}.txt", "wb")
+        print(f"Recibiendo archivo de Cliente {self.id}...")
+        self.startTime = time()
+        response = self.socket.recv(BUFFER_SIZE*64)
+        while True:
+            if 'ArchivoEnviado\'' not in str(response):
+                file1.write(response)
+                response = self.socket.recv(BUFFER_SIZE*64)
+            else:
+                break
+
+        file1.write(response[:-14])
+        file1.close()
+        self.tiempoTotal = time() - self.startTime
+        print(
+            f"Transmision y Escritura del archivo {self.nArchivo} Completada para el cliente {self.id}")
+        hashCode = sha256()
+        file2 = open(
+            f"ArchivosRecibidos/{date}-Cliente{self.id}-Prueba-{self.numeroConexiones}.txt", "rb")
+        hashCode.update(file2.read())
+        file2.close()
+        self.hashCalculado = hashCode.digest()
+        mensajeComprobacionHash = "La integridad del archivo es correcta" if self.hashCalculado == self.hashServidor else "La integridad del archivo no es correcta"
+        print(f"Hash Calculado {self.hashCalculado.hex()}")
+        print(f"Hash Recibido {self.hashServidor.hex()}")
+        if mensajeComprobacionHash == "La integridad del archivo es correcta":
+            print(
+                f"El archivo {self.nArchivo} del cliente {self.id} tiene comprobacion de integridad correcta.")
+        else:
+            print(
+                f"El archivo {self.nArchivo} del cliente {self.id} tiene comprobacion de integridad incorrecta.")
+
+        self.socket.send(mensajeComprobacionHash.encode())
+        file3 = open(f"Logs/{date} Cliente{self.id}.txt", "w")
+        file3.write(f"Archivo Recibido: {self.nArchivo.split('/')[1]}\n")
+        tamanioArchivo = path.getsize(
+            f"ArchivosRecibidos/{date}-Cliente{self.id}-Prueba-{self.numeroConexiones}.txt")
+        file3.write(f"Tamanio archivo recibido: {tamanioArchivo} bytes\n")
+        file3.write(f"Servidor con ip {host} y puerto {port}\n")
+        file3.write(f"Integridad del archivo: {mensajeComprobacionHash}\n")
+        file3.write(f"Tiempo de transmision: {self.tiempoTotal} segundos\n")
+        file3.close()
+        self.socket.close()
+
+
+print("\n\n----- Programa Cliente Servidor UDP -----\n\n")
+numeroDeClientes = ""
+while True:
+    numeroDeClientes = input("Numero de clientes (Threads): ")
+    if numeroDeClientes.isnumeric():
+        numeroDeClientes = int(numeroDeClientes)
+        if numeroDeClientes <= 0 or numeroDeClientes > 25:
+            print("Seleccione una opción valida")
+        else:
+            break
+    else:
+        print("Seleccione una opción valida")
+host = input("Ingrese IP del Servidor TCP (Si no sabe use el comando ifconfig): ")
+port = 8000
+arregloClientes = []
+for i in range(numeroDeClientes):
+    sckt = socket()
+    sckt.connect((host, port))
+    print(
+        f"Conexion del cliente {i} al servidor con ip {host} y puerto {port}")
+    thread = ThreadServidor(i, sckt)
+    arregloClientes.append(thread)
+for thread in arregloClientes:
+    thread.start()
+for thread in arregloClientes:
+    thread.join()
---
+++ Server/server.py
@@ -0,0 +1,141 @@
+import copy
+from socket import socket
+from threading import Thread
+from hashlib import sha256
+from time import time, sleep
+import os
+from datetime import datetime
+
+diccionarioComprobacionesHashArchivos = {}
+estadisticasTransmision = {}
+BUFFER_SIZE = 4096
+
+
+class ThreadCliente(Thread):
+    def __init__(self, id, socket, direccionCliente, numeroConexiones, nombreArchivo, bytesArchivo, hashArchivo):
+        Thread.__init__(self)
+        self.id = id
+        self.socket = socket
+        self.direccionCliente = direccionCliente
+        self.numeroConexiones = numeroConexiones
+        self.nombreArchivo = nombreArchivo
+        self.hashCode = hashArchivo
+        self.startEnvio = None
+        self.bytesArchivo = bytesArchivo
+        print(
+            f"Cliente creado con id {id}, ip {direccionCliente[0]} y puerto {direccionCliente[1]}")
+
+    def run(self):
+        global diccionarioComprobacionesHashArchivos, estadisticasTransmision
+        self.socket.recv(BUFFER_SIZE).decode()
+        self.socket.send(str(self.id).encode())
+        sleep(0.1)
+        self.socket.send(str(self.numeroConexiones).encode())
+        sleep(0.1)
+        self.socket.send(nArchivo.encode())
+        sleep(0.1)
+        self.socket.send(self.hashCode)
+        sleep(0.1)
+        self.startEnvio = time()
+        # Achivo en bytes
+        self.socket.send(self.bytesArchivo)
+        sleep(0.1)
+        self.socket.send('ArchivoEnviado'.encode())
+        sleep(0.1)
+        estadisticasTransmision[self.id] = time() - self.startEnvio
+        diccionarioComprobacionesHashArchivos[self.id] = self.socket.recv(BUFFER_SIZE).decode()
+        self.socket.close()
+        print(
+            f"Finalizacion envio de archivo al Cliente {self.id} con IP {self.direccionCliente[0]} y puerto {self.direccionCliente[1]}")
+
+
+print("------- Programa Servidor UDP -------\n")
+print("Recuerda ejecutar el comando 'truncate -s 100M 100MB.txt'")
+print("Recuerda ejecutar el comando 'truncate -s 250M 250MB.txt'")
+nArchivo = ""
+while True:
+    print("Presione 1 para el archivo de 100MB o 2 para el archivo de 250MB")
+    entrada = input()
+    if entrada.isnumeric():
+        if entrada == '1':
+            nArchivo = 'ArchivosAEnviar/100MB.txt'
+            break
+        elif entrada == '2':
+            nArchivo = 'ArchivosAEnviar/250MB.txt'
+            break
+        else:
+            print("Seleccione una opcion valida. Recuerde ejecutar el comando \n $'truncate -s 100M 100MB.txt' o \n $'truncate -s 250M 250MB.txt' para que funcione.")
+    else:
+        print("Seleccione una opcion valida")
+
+print("Archivo seleccionado correctamente.")
+file = open(nArchivo, "rb")
+bytesArchivo = file.read()
+file.close()
+print("Archivo cargado correctamente.")
+
+hashArchivo = sha256()
+hashArchivo.update(bytesArchivo)
+hashBytes = hashArchivo.digest()
+numeroDeClientes = ""
+while True:
+    numeroDeClientes = input("Numero de clientes (Threads): ")
+    if numeroDeClientes.isnumeric():
+        numeroDeClientes = int(numeroDeClientes)
+        if numeroDeClientes <= 0 or numeroDeClientes > 25:
+            print("Seleccione una opción valida")
+        else:
+            break
+    else:
+        print("Seleccione una opción valida")
+s = socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+host = '0.0.0.0'
+port = 8000
+s.bind((host, port))
+s.listen(5)
+print(
+    f"Servidor corriendo en el puerto {port} (Recuerde encontar la IP usando el comando ifconfig\n")
+
+
+arregloClientes = []
+arregloDirecciones = []
+
+for i in range(numeroDeClientes):
+    socketCliente, direccionCliente = s.accept()
+    print(
+        f"Conexion del cliente con ip {direccionCliente[0]} y puerto {direccionCliente[1]}")
+    t = ThreadCliente(i, socketCliente, direccionCliente,
+                      numeroDeClientes, nArchivo, copy.copy(bytesArchivo), hashBytes)
+    arregloClientes.append(t)
+    arregloDirecciones.append(direccionCliente)
+
+    if len(arregloClientes) == numeroDeClientes:
+        for t in arregloClientes:
+            t.start()
+
+        for t in arregloClientes:
+            t.join()
+
+        # Log
+        date = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        file = open(f"Logs/{date}.txt", "w")
+        tamanioArchivo = os.path.getsize(f"{nArchivo}")
+        file.write(
+            f"Archivo enviado: {nArchivo} - Tamanio en Bytes: {tamanioArchivo}")
+        file.write(
+            "\nIdentificacion por conexión del cliente al que se realiza la transferencia de archivos:\n")
+        for j in range(numeroDeClientes):
+            file.write(f"Cliente {j} con IP {arregloDirecciones[j][0]} y puerto {arregloDirecciones[j][1]}\n")
+        file.write("\n")
+        file.write("Resultados de la transferencia:\n")
+        for j in range(numeroDeClientes):
+            file.write(f"Cliente {j}: {diccionarioComprobacionesHashArchivos[j]}\n")
+        file.write("\n")
+
+        file.write("Tiempos de transmision:\n")
+        for j in range(numeroDeClientes):
+            file.write(
+                f"Cliente {j}: {estadisticasTransmision[j]} segundos\n")
+        file.write("\n")
+        file.close()
+        s.close()
